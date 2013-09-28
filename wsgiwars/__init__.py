ed0f0041ec81446ebd2245f3fff317a1cd5ba1ab
Cyprien Le Pannérer <cyplp@free.fr>
2013-09-28 09:14:32 +0200

    better signup

--- wsgiwars/__init__.py
+++ wsgiwars/__init__.py
@@ -20,6 +20,7 @@ def main(global_config, **settings):
 
     config.add_route('home', '/')
     config.add_route('signup', '/signup')
+    config.add_route('submitSignup', '/signup/submit')
     config.add_route('login', '/login')
 
     config.include('pyramid_fanstatic')
