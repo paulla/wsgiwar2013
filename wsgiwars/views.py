a3f33601bb502889701e33211559f962205c1ba2
Cyprien Le Pannérer <cyplp@free.fr>
2013-09-28 09:54:15 +0200

    Ready for mika

--- wsgiwars/views.py
+++ wsgiwars/views.py
@@ -11,14 +11,29 @@ def signup(request):
     return {}
 
 
-@view_config(route_name='submitSignup', renderer='templates/submitSignup.pt')
+@view_config(route_name='submitSignup', renderer='templates/signupSubmit.pt')
 def submitSignup(request):
 
     # TODO check is username isn't taken
-    if request.matchdict['password'] == request.matchdict['confirmPassword']:
+    if request.POST['password'] == request.POST['confirmPassword']:
         # register user
         # Mika64 : your job !
-        pass
+
+
+        from pyramid_mailer.mailer import Mailer
+        from pyramid_mailer.message import Message
+
+        mailer = Mailer()
+        message = Message(subject="Your subsription !",
+                          sender=request.registry.settings['mail_from'],
+                          recipients=[request.POST['email']],
+                          body="Confirm the link") # TODO add link
+
+
+        mailer.send(message)
+
+        return {}
+
     else:
         # redo :/
         pass
