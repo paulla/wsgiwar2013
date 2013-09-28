545a37cfe65a0f7660a5fa6c679e1f533a32376d
Cyprien Le Pannérer <cyplp@free.fr>
2013-09-28 09:40:48 +0200

    skeleton of submmit

--- wsgiwars/views.py
+++ wsgiwars/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 
+
 @view_config(route_name='home', renderer='templates/home.pt')
 def home(request):
     return {'project': 'wsgiwars'}
@@ -8,4 +9,19 @@ def home(request):
 @view_config(route_name='signup', renderer='templates/signup.pt')
 def signup(request):
     return {}
+
+
+@view_config(route_name='submitSignup', renderer='templates/submitSignup.pt')
+def submitSignup(request):
+
+    # TODO check is username isn't taken
+    if request.matchdict['password'] == request.matchdict['confirmPassword']:
+        # register user
+        # Mika64 : your job !
+        pass
+    else:
+        # redo :/
+        pass
+    return {}
+
 
