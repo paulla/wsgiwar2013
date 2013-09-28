a4596b8151b424694c0d0b4f72c53b0382ee4957
Cyprien Le Pannérer <cyplp@free.fr>
2013-09-28 08:39:03 +0200

    begin of signup

--- wsgiwars/views.py
+++ wsgiwars/views.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/home.pt')
 def home(request):
     return {'project': 'wsgiwars'}
+
+@view_config(route_name='signup', renderer='templates/signup.pt')
+def signup(request):
+    return {}
+
