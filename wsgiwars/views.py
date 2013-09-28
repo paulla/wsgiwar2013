ce9e9b632453a5fefd374ac53078cc7e6777e46d
Michaël Ricart <ricart.michael@gmail.com>
2013-09-28 10:39:22 +0200

    register user in db

--- wsgiwars/views.py
+++ wsgiwars/views.py
@@ -1,5 +1,10 @@
 from pyramid.view import view_config
 
+import bcrypt
+
+import couchdbkit
+
+from paulla.wsgiwar2013.models.users import Users_db
 
 
 @view_config(route_name='home', renderer='templates/home.pt')
@@ -18,7 +23,16 @@ def submitSignup(request):
     if request.POST['password'] == request.POST['confirmPassword']:
         # register user
         # Mika64 : your job !
+        password = bcrypt.hashpw(request.POST['password'], bcrypt.gensalt())
 
+        user = Users_db(username = request.POST['username'],
+                password = password,
+                avatar = request.POST['avatar'],
+                name = request.POST['name']
+                description = request.POST['description']
+                )
+
+        user.save()
 
         from pyramid_mailer.mailer import Mailer
         from pyramid_mailer.message import Message
